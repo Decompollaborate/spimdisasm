0df73a0524c9f24e916f3f85c733bea2ea6f7eb2
Elliptic Ellipsis <elliptic.ellipsis@gmail.com>
2021-11-12 18:19:14 +0000

    Improve shell script to take dir and version, print usage

--- pika_disasm.sh
+++ pika_disasm.sh
@@ -1,3 +1,22 @@
-./simpleDisasm.py ../hypdisasm/baserom/usa/boot.bin pikachu/boot  --vram 80000450 --functions ../hypdisasm/tables/usa/functions.csv --variables ../hypdisasm/tables/usa/variables.csv --file-splits ../hypdisasm/tables/usa/files_boot.csv --save-context pikachu/context_boot.txt
-./simpleDisasm.py ../hypdisasm/baserom/usa/file_B3C70.bin  pikachu/file_B3C70   --vram 8010AAF0 --functions ../hypdisasm/tables/usa/functions.csv --variables ../hypdisasm/tables/usa/variables.csv --file-splits ../hypdisasm/tables/usa/files_file_B3C70.csv --save-context pikachu/context_B3C70.txt
-./simpleDisasm.py ../hypdisasm/baserom/usa/file_DCF60.bin pikachu/file_DCF60   --vram 80133DE0 --functions ../hypdisasm/tables/usa/functions.csv --variables ../hypdisasm/tables/usa/variables.csv  --save-context pikachu/context_DCF60.txt
+#!/bin/bash
+
+if [[ $# -le 1 ]]
+then
+    echo "Usage: $0 BASE_DIRECTORY [usa|jp]"
+    exit 1
+fi
+
+DIR=$1
+VERSION=$2
+
+FUNCTIONS="${DIR}/tables/${VERSION}/functions.csv"
+VARIABLES="${DIR}/tables/${VERSION}/variables.csv"
+
+BASEROM_DIR="${DIR}/baserom/${VERSION}"
+ASM_DIR="${DIR}/asm/${VERSION}"
+TABLES_DIR="${DIR}/tables/${VERSION}"
+CONTEXT_DIR="${DIR}/context/${VERSION}"
+
+./simpleDisasm.py "${BASEROM_DIR}/boot.bin" "${ASM_DIR}/boot" --vram 80000450 --functions ${FUNCTIONS} --variables ${VARIABLES} --file-splits "${TABLES_DIR}/files_boot.csv" --save-context "${CONTEXT_DIR}/boot.txt"
+./simpleDisasm.py "${BASEROM_DIR}/file_B3C70.bin" "${ASM_DIR}/file_B3C70" --vram 8010AAF0 --functions ${FUNCTIONS} --variables ${VARIABLES} --file-splits "${TABLES_DIR}/files_file_B3C70.csv" --save-context "${CONTEXT_DIR}/B3C70.txt"
+./simpleDisasm.py "${BASEROM_DIR}/file_DCF60.bin" "${ASM_DIR}/file_DCF60" --vram 80133DE0 --functions ${FUNCTIONS} --variables ${VARIABLES} --save-context "${CONTEXT_DIR}/DCF60.txt"
