498e6ba8a82ec609f504976f0b7bf547ba98db94
Angie <angheloalf95@gmail.com>
2021-11-15 22:10:20 -0300

    bss

--- mips/MipsBss.py
+++ mips/MipsBss.py
@@ -6,9 +6,21 @@ from .Utils import *
 from .GlobalConfig import GlobalConfig
 from .MipsFileBase import FileBase
 from .MipsSection import Section
+from .MipsContext import Context
 
 
 class Bss(Section):
+    def __init__(self, bssVramStart: int, bssVramEnd: int, filename: str, version: str, context: Context):
+        super().__init__(bytearray(), filename, version, context)
+
+        self.bssVramStart: int = bssVramStart
+        self.bssVramEnd: int = bssVramEnd
+
+
+    def analyze(self):
+        if self.context is not None:
+            self.context.symbols
+
     def removePointers(self) -> bool:
         if not GlobalConfig.REMOVE_POINTERS:
             return False
@@ -19,16 +31,42 @@ class Bss(Section):
     def saveToFile(self, filepath: str):
         super().saveToFile(filepath + ".bss")
 
-        if self.size == 0:
+        if self.context is None:
             return
 
         with open(filepath + ".bss.s", "w") as f:
-            # f.write(".section .bss\n\n.balign 16\n\n")
-            offset = 0
-            for w in self.words:
-                offsetHex = toHex(offset, 5)[2:]
-                bssHex = toHex(w, 8)[2:]
-                line = toHex(w, 8)
+            f.write(".include \"macro.inc\"\n")
+            f.write("\n")
+            f.write("# assembler directives\n")
+            f.write(".set noat      # allow manual use of $at\n")
+            f.write(".set noreorder # don't insert nops after branches\n")
+            f.write(".set gp=64     # allow use of 64-bit general purpose registers\n")
+            f.write("\n")
+            f.write(".section .bss\n")
+            f.write("\n")
+            f.write(".balign 16\n")
 
-                f.write(f"/* {offsetHex} {bssHex} */  .word  {line}\n")
+            offset = 0
+            inFileOffset = self.offset
+            sortedSymbols = sorted(self.context.symbols.items())
+            i = 0
+            while i < len(sortedSymbols):
+                symbolVram, symbol = sortedSymbols[i]
+                if symbolVram < self.bssVramStart:
+                    i += 1
+                    continue
+                if symbolVram >= self.bssVramEnd:
+                    break
+
+                offsetHex = toHex(inFileOffset + self.commentOffset, 6)[2:]
+                vramHex = toHex(symbolVram, 8)[2:]
+
+                space = self.bssVramEnd - symbolVram
+                if i + 1 < len(sortedSymbols):
+                    space = sortedSymbols[i+1][0] - symbolVram
+
+                label = f"\nglabel {symbol.name}\n"
+                f.write(f"{label}/* {offsetHex} {vramHex} */  .space  {toHex(space, 2)}\n")
                 offset += 4
+                inFileOffset += 4
+                i += 1
