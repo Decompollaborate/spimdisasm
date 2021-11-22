9d583430ab95acb02c5b3bcf430c0c1a83ff463f
Angie <angheloalf95@gmail.com>
2021-11-22 16:45:25 -0300

    Fix relative imports

--- mips/MipsContext.py
+++ mips/MipsContext.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 import ast
 from sortedcontainers import SortedDict
 
-from mips.GlobalConfig import GlobalConfig
+from .GlobalConfig import GlobalConfig
 
 from .Utils import *
 
