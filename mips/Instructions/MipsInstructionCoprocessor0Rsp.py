edf748e58b02bce98c1b31eba06a07d979871738
angie <angheloalf95@gmail.com>
2022-01-12 19:31:17 -0300

    Implement Gpr and Cop0 RSP registers

--- mips/Instructions/MipsInstructionCoprocessor0Rsp.py
+++ mips/Instructions/MipsInstructionCoprocessor0Rsp.py
@@ -35,8 +35,8 @@ class InstructionCoprocessor0Rsp(InstructionCoprocessor0):
         formated_opcode = opcode.lower().ljust(self.ljustWidthOpcode, ' ')
         # rt = self.getRegisterName(self.rt)
         # rd = self.getCop0RegisterName(self.rd)
-        rt = f"${self.rt}"
-        rd = f"${self.rd}"
+        rt = self.getGprRspRegisterName(self.rt)
+        rd = self.getCop0RspRegisterName(self.rd)
 
         if not self.isImplemented():
             return super().disassemble(context, immOverride)
