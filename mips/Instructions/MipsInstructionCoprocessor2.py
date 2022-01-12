aa0b2beff1a946fcc931c2f6bfe74436d3c1860f
angie <angheloalf95@gmail.com>
2022-01-12 02:16:57 -0300

    Implement a few more cop2 instructions

--- mips/Instructions/MipsInstructionCoprocessor2.py
+++ mips/Instructions/MipsInstructionCoprocessor2.py
@@ -15,18 +15,25 @@ class InstructionCoprocessor2(InstructionBase):
     Cop2Opcodes: Dict[int, InstructionVectorId] = {
         0x00: InstructionVectorId.VMULF,
         0x01: InstructionVectorId.VMULU,
+        0b000_010: InstructionVectorId.VRNDP,
+        0b000_011: InstructionVectorId.VMULQ,
         0x04: InstructionVectorId.VMUDL,
         0x05: InstructionVectorId.VMUDM,
         0x06: InstructionVectorId.VMUDN,
         0x07: InstructionVectorId.VMUDH,
         0x08: InstructionVectorId.VMACF,
         0x09: InstructionVectorId.VMACU,
+        0b001_010: InstructionVectorId.VRNDN,
+        0b001_011: InstructionVectorId.VMACQ,
         0x0C: InstructionVectorId.VMADL,
         0x0D: InstructionVectorId.VMADM,
         0x0E: InstructionVectorId.VMADN,
         0x0F: InstructionVectorId.VMADH,
         0x10: InstructionVectorId.VADD,
+        0b010_001: InstructionVectorId.VSUB,
+        0b010_011: InstructionVectorId.VABS,
         0x14: InstructionVectorId.VADDC,
+        0b010_101: InstructionVectorId.VSUBC,
         0x1D: InstructionVectorId.VSAR,
         0x28: InstructionVectorId.VAND,
         0x29: InstructionVectorId.VNAND,
@@ -43,6 +50,15 @@ class InstructionCoprocessor2(InstructionBase):
         0x25: InstructionVectorId.VCH,
         0x26: InstructionVectorId.VCR,
         0x27: InstructionVectorId.VMRG,
+
+        0b110_000: InstructionVectorId.VRCP,
+        0b110_001: InstructionVectorId.VRCPL,
+        0b110_010: InstructionVectorId.VRCPH,
+        0b110_011: InstructionVectorId.VMOV,
+        0b110_100: InstructionVectorId.VRSQ,
+        0b110_101: InstructionVectorId.VRSQL,
+        0b110_110: InstructionVectorId.VRSQH,
+        0b110_111: InstructionVectorId.VNOP,
     }
     Cop2MoveOpcodes: Dict[int, InstructionVectorId] = {
         0b00_000: InstructionVectorId.MFC2,
@@ -91,12 +107,21 @@ class InstructionCoprocessor2(InstructionBase):
         vs = f"${self.vs}"
         vd = f"${self.vd}"
 
-        result = ""
+        result = formated_opcode
+
+        if self.uniqueId == InstructionVectorId.VNOP:
+            return opcode.lower()
+
+        if self.uniqueId in (InstructionVectorId.VMOV, InstructionVectorId.VRCP, InstructionVectorId.VRCPH, InstructionVectorId.VRSQ, InstructionVectorId.VRSQH, InstructionVectorId.VRSQL):
+            result += f" {vd}[{self.vs}], "
+            result = result.ljust(14, ' ')
+            result += f" {vt}[{e}]"
+            return result
 
         if e_upper == 0:
             rt = f"${self.rt}"
             rd = f"${self.rd}"
-            result = f"{formated_opcode} {rt},"
+            result += f" {rt},"
             result = result.ljust(14, ' ')
             if self.uniqueId in (InstructionVectorId.CFC2, InstructionVectorId.CTC2):
                 result += f" {rd}"
@@ -106,7 +131,7 @@ class InstructionCoprocessor2(InstructionBase):
                 # TODO: use vector register instead of rd
                 result += f" {rd}[{index}]"
         else:
-            result = f"{formated_opcode} {vd},"
+            result += f" {vd},"
             result = result.ljust(14, ' ')
             result += f" {vs},"
             result = result.ljust(19, ' ')
@@ -115,6 +140,6 @@ class InstructionCoprocessor2(InstructionBase):
                 # TODO: do this properly
                 result += f"[{e}]"
 
-        if self.isImplemented():
+        if not self.isImplemented():
             result = "ERROR # " + result
         return result
