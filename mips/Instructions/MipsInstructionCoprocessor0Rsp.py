9bbb9aaa592abdd01e51360e83ad17869688cd98
angie <angheloalf95@gmail.com>
2022-01-12 15:35:26 -0300

    Rsp cop0 instructions

---
+++ mips/Instructions/MipsInstructionCoprocessor0Rsp.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+from __future__ import annotations
+
+from .MipsConstants import InstructionId, InstructionVectorId
+
+from ..Utils import *
+
+from .MipsInstructionCoprocessor0 import InstructionCoprocessor0
+from ..GlobalConfig import GlobalConfig
+from ..MipsContext import Context
+
+
+class InstructionCoprocessor0Rsp(InstructionCoprocessor0):
+    Cop0Opcodes_ByFormat = {
+        0b00_000: InstructionId.MFC0,
+        0b00_100: InstructionId.MTC0,
+    }
+
+    def __init__(self, instr: int):
+        super().__init__(instr)
+
+        # self.opcodesDict = 
+        self.processUniqueId()
+
+
+    def processUniqueId(self):
+        super().processUniqueId()
+
+        self.uniqueId = self.Cop0Opcodes_ByFormat.get(self.fmt, InstructionId.INVALID)
+
+
+    def disassemble(self, context: Context|None, immOverride: str|None=None) -> str:
+        opcode = self.getOpcodeName()
+        formated_opcode = opcode.lower().ljust(self.ljustWidthOpcode, ' ')
+        # rt = self.getRegisterName(self.rt)
+        # rd = self.getCop0RegisterName(self.rd)
+        rt = f"${self.rt}"
+        rd = f"${self.rd}"
+
+        if not self.isImplemented():
+            return super().disassemble(context, immOverride)
+
+        result = f"{formated_opcode} {rt},"
+        result = result.ljust(14, ' ')
+        result += f" {rd}"
+        return result
