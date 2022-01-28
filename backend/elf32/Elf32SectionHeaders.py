78155ca4356fcf00d82e53cf494537b397105a90
angie <angheloalf95@gmail.com>
2022-01-28 03:10:48 -0300

    Use the function names in the .o disassembler

--- backend/elf32/Elf32SectionHeaders.py
+++ backend/elf32/Elf32SectionHeaders.py
@@ -40,3 +40,9 @@ class Elf32SectionHeaders:
             self.sections.append(sectionHeaderEntry)
             # print(sectionHeaderEntry)
 
+    def __getitem__(self, key: int) -> Elf32SectionHeaderEntry:
+        return self.sections[key]
+
+    def __iter__(self):
+        for entry in self.sections:
+            yield entry
