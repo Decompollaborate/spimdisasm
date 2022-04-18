a21c98eaaebdfb16ece6ffc148f8e6ec419a5599
angie <angheloalf95@gmail.com>
2022-04-17 21:57:13 -0400

    Refactor RelocZ64 to accomodate the rest of the codebase

--- backend/mips/MipsRelocZ64.py
+++ backend/mips/MipsRelocZ64.py
@@ -10,9 +10,11 @@ from ..common.Utils import *
 
 from ..common.GlobalConfig import GlobalConfig
 from ..common.Context import Context
+from ..common.FileSectionType import FileSectionType, FileSections_ListBasic
 
 from .MipsFileBase import FileBase
 from .MipsSection import Section
+from .MipsRelocTypes import RelocTypes
 
 
 class RelocEntry:
@@ -48,18 +50,21 @@ class RelocEntry:
     def __str__(self) -> str:
         section = self.getSectionName()
         reloc = self.getTypeName()
-        return f"{section} {reloc} {hex(self.offset)}"
+        return f"{section} {reloc} 0x{self.offset:X}"
     def __repr__(self) -> str:
         return self.__str__()
+
 
 class RelocZ64(Section):
     def __init__(self, array_of_bytes: bytearray, filename: str, context: Context):
         super().__init__(array_of_bytes, filename, context)
 
-        self.textSize = self.words[0]
-        self.dataSize = self.words[1]
-        self.rodataSize = self.words[2]
-        self.bssSize = self.words[3]
+        self.sectionSizes = {
+            FileSectionType.Text: self.words[0],
+            FileSectionType.Data: self.words[1],
+            FileSectionType.Rodata: self.words[2],
+            FileSectionType.Bss: self.words[3],
+        }
         self.relocCount = self.words[4]
 
         self.tail = self.words[self.relocCount+5:-1]
@@ -71,8 +76,37 @@ class RelocZ64(Section):
             self.entries.append(RelocEntry(word))
 
     @property
-    def nRelocs(self):
+    def nRelocs(self) -> int:
         return len(self.entries)
+
+    @property
+    def textSize(self) -> int:
+        return self.sectionSizes[FileSectionType.Text]
+    @property
+    def dataSize(self) -> int:
+        return self.sectionSizes[FileSectionType.Data]
+    @property
+    def rodataSize(self) -> int:
+        return self.sectionSizes[FileSectionType.Rodata]
+    @property
+    def bssSize(self) -> int:
+        return self.sectionSizes[FileSectionType.Bss]
+
+
+    def analyze(self):
+        if self.vRamStart > -1:
+            currentVram = self.getVramOffset(0)
+            if self.context.getSymbol(currentVram, False) is None:
+                self.context.addSymbol(currentVram, f"{self.filename}_OverlayInfo")
+
+            currentVram += 0x14
+            if self.context.getSymbol(currentVram, False) is None:
+                self.context.addSymbol(currentVram, f"{self.filename}_OverlayRelocations")
+
+            currentVram = self.getVramOffset(self.size - 4)
+            if self.context.getSymbol(currentVram, False) is None:
+                self.context.addSymbol(currentVram, f"{self.filename}_OverlayInfoOffset")
+
 
     def compareToFile(self, other_file: FileBase):
         result = super().compareToFile(other_file)
@@ -92,7 +126,7 @@ class RelocZ64(Section):
             return False
 
         # TODO ?
-        #super().removePointers()
+        # super().removePointers()
         return False
 
 
@@ -100,48 +134,135 @@ class RelocZ64(Section):
         result = ""
 
         offset = 0
+        inFileOffset = self.offset
         currentVram = self.getVramOffset(offset)
 
-        result += f"glabel {self.filename}OverlayInfo\n"
+        label = f"\nglabel {self.filename}OverlayInfo\n"
+        # if we have vram available, try to get the symbol name from the Context
+        if self.vRamStart > -1:
+            auxLabel = self.context.getAnySymbol(currentVram)
+            if auxLabel is not None:
+                label = "\nglabel " + auxLabel.getSymbolPlusOffset(currentVram) + "\n"
 
-        result += f"/* %05X %08X %08X */  .word _{self.filename}SegmentTextSize # 0x{self.textSize:02X}\n" % (offset + self.commentOffset + 0x0, currentVram + 0x0, self.textSize)
-        result += f"/* %05X %08X %08X */  .word _{self.filename}SegmentDataSize # 0x{self.dataSize:02X}\n" % (offset + self.commentOffset + 0x4, currentVram + 0x4, self.dataSize)
-        result += f"/* %05X %08X %08X */  .word _{self.filename}SegmentRoDataSize # 0x{self.rodataSize:02X}\n" % (offset + self.commentOffset + 0x8, currentVram + 0x8, self.rodataSize)
-        result += f"/* %05X %08X %08X */  .word _{self.filename}SegmentBssSize # 0x{self.bssSize:02X}\n" % (offset + self.commentOffset + 0xC, currentVram + 0xC, self.bssSize)
-        result += f"\n"
-        result += f"/* %05X %08X %08X */  .word  {self.relocCount} # reloc_count\n" % (offset + self.commentOffset + 0x10, currentVram + 0x10, self.relocCount)
-        result += f"\n"
+        result += label
 
-        offset += 0x14
+        for fileSect in FileSections_ListBasic:
+            offsetHex = f"{inFileOffset + self.commentOffset:06X}"
 
-        result += f"glabel {self.filename}OverlayRelocations\n"
-        for r in self.entries:
-            offsetHex = toHex(offset + self.commentOffset, 5)[2:]
             vramHex = ""
-            if self.vRamStart != -1:
-                currentVram = self.getVramOffset(offset)
-                vramHex = toHex(currentVram, 8)[2:]
-            relocHex = toHex(r.reloc, 8)[2:]
-            line = str(r)
+            if self.vRamStart > -1:
+                vramHex = f"{currentVram:08X}"
 
-            result += f"/* {offsetHex} {vramHex} {relocHex} */  .word 0x{relocHex} # {line}\n"
+            sectName = fileSect.toCapitalizedStr()
+            value = f"_{self.filename}Segment{sectName}Size"
+
+            comment = ""
+            if GlobalConfig.ASM_COMMENT:
+                dataHex = f"{self.sectionSizes[fileSect]:08X}"
+                comment = f"/* {offsetHex} {vramHex} {dataHex} */"
+
+            line = f"{comment} .word {value}\n"
+            result += line
+
             offset += 4
+            inFileOffset += 4
+            currentVram += 4
+
+        result += f"\n"
+
+        offsetHex = f"{inFileOffset + self.commentOffset:06X}"
+
+        vramHex = ""
+        if self.vRamStart > -1:
+            vramHex = f"{currentVram:08X}"
+
+        value = f"{self.relocCount} # reloc_count"
+
+        comment = ""
+        if GlobalConfig.ASM_COMMENT:
+            dataHex = f"{self.relocCount:08X}"
+            comment = f"/* {offsetHex} {vramHex} {dataHex} */"
+
+        line = f"{comment} .word {value}\n"
+        result += line
+
+        offset += 4
+        inFileOffset += 4
+        currentVram += 4
+
+
+        label = f"\nglabel {self.filename}OverlayRelocations\n"
+
+        if self.vRamStart > -1:
+            auxLabel = self.context.getAnySymbol(currentVram)
+            if auxLabel is not None:
+                label = "\nglabel " + auxLabel.getSymbolPlusOffset(currentVram) + "\n"
+
+        result += label
+
+        for r in self.entries:
+            offsetHex = f"{inFileOffset + self.commentOffset:06X}"
+            vramHex = ""
+            if self.vRamStart > -1:
+                vramHex = f"{currentVram:08X}"
+
+            relocHex = f"{r.reloc:08X}"
+
+            comment = ""
+            if GlobalConfig.ASM_COMMENT:
+                comment = f"/* {offsetHex} {vramHex} {relocHex} */"
+
+            line = f"{comment} .word 0x{relocHex} # {str(r)}\n"
+            result += line
+
+            offset += 4
+            inFileOffset += 4
+            currentVram += 4
 
         result += "\n"
         for pad in self.tail:
-            offsetHex = toHex(offset + self.commentOffset, 5)[2:]
+            offsetHex = f"{inFileOffset + self.commentOffset:06X}"
             vramHex = ""
-            if self.vRamStart != -1:
-                currentVram = self.getVramOffset(offset)
-                vramHex = toHex(currentVram, 8)[2:]
-            padcHex = toHex(pad, 8)
+            if self.vRamStart > -1:
+                vramHex = f"{currentVram:08X}"
 
-            f"/* {offsetHex} {vramHex} {padcHex[2:]} */  .word {padcHex}\n"
+            padcHex = f"{pad:08X}"
+
+            comment = ""
+            if GlobalConfig.ASM_COMMENT:
+                comment = f"/* {offsetHex} {vramHex} {padcHex} */"
+
+            line = f"{comment} .word 0x{padcHex}\n"
+            result += line
+
             offset += 4
+            inFileOffset += 4
+            currentVram += 4
 
-        result += f"glabel {self.filename}OverlayInfoOffset\n"
-        currentVram = self.getVramOffset(offset)
-        result += f"/* %05X %08X %08X */  .word  0x{self.seekup:02X}\n" % (offset + 0x0, currentVram + 0x0, self.seekup)
+        label = f"\nglabel {self.filename}OverlayInfoOffset\n"
+
+        if self.vRamStart > -1:
+            auxLabel = self.context.getAnySymbol(currentVram)
+            if auxLabel is not None:
+                label = "\nglabel " + auxLabel.getSymbolPlusOffset(currentVram) + "\n"
+
+        result += label
+
+        offsetHex = f"{inFileOffset + self.commentOffset:06X}"
+
+        vramHex = ""
+        if self.vRamStart > -1:
+            vramHex = f"{currentVram:08X}"
+
+        value = f"0x{self.seekup:02X}"
+
+        comment = ""
+        if GlobalConfig.ASM_COMMENT:
+            dataHex = f"{self.seekup:08X}"
+            comment = f"/* {offsetHex} {vramHex} {dataHex} */"
+
+        line = f"{comment} .word {value}\n"
+        result += line
 
         return result
 
