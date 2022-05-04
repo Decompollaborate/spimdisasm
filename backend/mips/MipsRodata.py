b6acd9982413bd342dbc3abafa45ecff0a5e44df
angie <angheloalf95@gmail.com>
2022-05-03 23:44:45 -0400

    Minor rodata fix

--- backend/mips/MipsRodata.py
+++ backend/mips/MipsRodata.py
@@ -18,7 +18,8 @@ class Rodata(Section):
     def __init__(self, context: Context, vram: int|None, filename: str, array_of_bytes: bytearray):
         super().__init__(context, vram, filename, array_of_bytes, FileSectionType.Rodata)
 
-        self.bytes: bytearray = array_of_bytes
+        self.bytes: bytearray = bytearray(self.sizew*4)
+        beWordsToBytes(self.words, self.bytes)
 
         # addresses of symbols in this rodata section
         self.symbolsVRams: Set[int] = set()
