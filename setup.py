a315dd1debd46c77e27e624f045859f078956f4e
Ethan Roseman <ethteck@gmail.com>
2022-05-26 03:12:25 -0400

    Add CI which builds a wheel and publishes it to PyPI (#17)

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup # type: ignore
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="spimdisasm",
+    version="1.0.3",
+    author="Decompollaborate",
+    license="MIT",
+    description="N64 MIPS disassembler",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
