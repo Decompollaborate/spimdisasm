fa50903ab544d725bd5be2f777905e9445a96545
angie <angheloalf95@gmail.com>
2022-05-28 22:40:28 -0400

    version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="spimdisasm",
-    version="1.0.6",
+    version="1.1.0",
     author="Decompollaborate",
     license="MIT",
     description="N64 MIPS disassembler",
