b05a6bd7b9a2194f7e0f73b107c58ae0e7c79c69
angie <angheloalf95@gmail.com>
2022-10-28 15:15:05 -0300

    move gpword logic a bit

--- spimdisasm/mips/symbols/MipsSymbolRodata.py
+++ spimdisasm/mips/symbols/MipsSymbolRodata.py
@@ -208,12 +208,11 @@ class SymbolRodata(SymbolBase):
             if self.contextSym.isJumpTable() and self.contextSym.isGot and common.GlobalConfig.GP_VALUE is not None:
                 labelAddr = common.GlobalConfig.GP_VALUE + rabbitizer.Utils.from2Complement(w, 32)
                 labelSym = self.getSymbol(labelAddr, tryPlusOffset=False)
+                dotType = ".gpword"
             else:
                 labelSym = self.getSymbol(w, tryPlusOffset=False)
             if labelSym is not None:
                 value = labelSym.getName()
-                if self.contextSym.isJumpTable() and common.GlobalConfig.PIC:
-                    dotType = ".gpword"
             elif self.isString():
                 try:
                     buffer = bytearray(4*len(self.words))
