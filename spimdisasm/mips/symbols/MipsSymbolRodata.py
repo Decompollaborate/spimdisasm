36b043c94ef864308a556544482470e9b75ea0cf
Angie <angheloalf95@gmail.com>
2022-10-29 12:32:43 -0300

    Remove `w != 0` check

--- spimdisasm/mips/symbols/MipsSymbolRodata.py
+++ spimdisasm/mips/symbols/MipsSymbolRodata.py
@@ -205,7 +205,7 @@ class SymbolRodata(SymbolBase):
             rodataWord = doubleWord
             skip = 1
         else:
-            if self.contextSym.isJumpTable() and self.contextSym.isGot and common.GlobalConfig.GP_VALUE is not None and w != 0:
+            if self.contextSym.isJumpTable() and self.contextSym.isGot and common.GlobalConfig.GP_VALUE is not None:
                 labelAddr = common.GlobalConfig.GP_VALUE + rabbitizer.Utils.from2Complement(w, 32)
                 labelSym = self.getSymbol(labelAddr, tryPlusOffset=False)
                 if labelSym is not None:
