13943bf1a45faeecb344956e56afa14505182589
angie <angheloalf95@gmail.com>
2024-02-06 21:28:23 -0300

    Data symbols: Emit the original word inside the emitted comment.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -17,6 +17,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Changed
 
+- Data symbols: Emit the original word inside the emitted comment.
 - Force `SectionRelocZ64`'s generated symbols to always be disassembled as
   `.word`s.
 - Disallow `SectionRelocZ64`'s generated symbols to reference other symbols or
--- spimdisasm/mips/symbols/MipsSymbolBase.py
+++ spimdisasm/mips/symbols/MipsSymbolBase.py
@@ -382,7 +382,7 @@ class SymbolBase(common.ElementBase):
                     if constant is not None:
                         value = constant.getName()
 
-        comment = self.generateAsmLineComment(localOffset)
+        comment = self.generateAsmLineComment(localOffset, w)
         output += f"{label}{comment} {dotType} {value}"
         output += self.getEndOfLineComment(i)
         output += common.GlobalConfig.LINE_ENDS
